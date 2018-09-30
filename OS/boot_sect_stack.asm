edbd31649c75063534e7393dfc936be5d077cdf2
vulkrim <vulkrim@gmail.com>
2018-09-30 11:12:25 -0700

    added OS project folder contains bins and asm files

---
+++ OS/boot_sect_stack.asm
@@ -0,0 +1,53 @@
+mov ah, 0x0e
+
+mov bp, 0x8000
+mov sp, bp
+
+push 'A'
+push 'B'
+push 'C'
+push 'D'
+push 'E'
+
+mov al, [0x7fff]
+int 0x10
+
+mov al, [0x7ffe]
+int 0x10
+
+mov al, [0x7ffd]
+int 0x10
+
+mov al, [0x7ffc]
+int 0x10
+
+mov al, [0x7ffb]
+int 0x10
+
+mov al, [0x7ffa]
+int 0x10
+
+mov al, [0x7ff9]
+int 0x10
+
+mov al, [0x7ff8]
+int 0x10
+
+mov al, [0x7ff7]
+int 0x10
+
+pop bx
+mov al,bl
+int 0x10
+
+pop bx
+mov al,bl
+int 0x10
+
+pop bx
+mov al,bl
+int 0x10
+
+jmp $
+times 510-($-$$) db 0
+dw 0xaa55
